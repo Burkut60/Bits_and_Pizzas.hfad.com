6cfe1c62b75a6501b991c2239e3a0669b0a85b37
Burkut60 <=kamilla2016@111>
2021-01-23 22:41:21 +0300

    add 2 activity order

--- app/src/main/java/com/hfad/example/bitsandpizzashfadcom/OrderActivity.java
+++ app/src/main/java/com/hfad/example/bitsandpizzashfadcom/OrderActivity.java
@@ -1,18 +1,17 @@
 package com.hfad.example.bitsandpizzashfadcom;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class OrderActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-       Toolbar toolbar = findViewById(R.id.toolbar);
-       setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_order);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
